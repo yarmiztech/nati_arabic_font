efd20a809b4a586505259b73316c140d2324cc05
enzapps <snippebucket@gmail.com>
2023-10-30 11:53:43 +0530

    <yt>

---
+++ __manifest__.py
@@ -0,0 +1,38 @@
+ # -*- coding: utf-8 -*-
+{
+    'name': "Arabic Font",
+
+    'summary': """
+        change defult font to nice arabic font""",
+
+    'description': """
+        Change the defult arabic font of the all interfaces with a beautiful one preferred by the Arabic user
+ ,
+    """,
+    'author': "Enzapps Private Limited",
+    'website': "http://enzapps.com",
+    'category': 'Localization',
+    'version': '16.0.0.0',
+    'depends': ['web'],
+    'qweb': [],
+
+    'images': ['static/description/banner.png'],
+    'license': 'LGPL-3',
+    'auto_install': True,
+    'installable': True,
+    'assets': {
+        'web.assets_common': [
+            'nati_arabic_font/static/src/scss/almaraifont.scss',
+            'nati_arabic_font/static/src/scss/cairofont.scss',
+            'nati_arabic_font/static/src/scss/droidfont.scss',
+            'nati_arabic_font/static/src/css/web_style.css',
+        ],
+        'report_assets_common': [
+            'nati_arabic_font/static/src/scss/almaraifont.scss',
+            'nati_arabic_font/static/src/scss/cairofont.scss',
+            'nati_arabic_font/static/src/scss/droidfont.scss',
+        ],
+
+
+    },
+}
